3fe8d7baae74818298c09adb17e86376f29c0fa0
qn5566 <sohotguys@gmail.com>
2022-05-18 11:23:32 +0800

    更新API

--- app/src/main/java/com/meetstudio/pharmacy/Constants.kt
+++ app/src/main/java/com/meetstudio/pharmacy/Constants.kt
@@ -1,6 +1,6 @@
 package com.meetstudio.pharmacy
 
-const val PHARMACIES_DATA_URL = "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
+const val PHARMACIES_DATA_URL = "https://raw.githubusercontent.com/qn5566/Pharmacy/master/data/info.json"
 
 const val REQUEST_LOCATION_PERMISSION = 1
 
